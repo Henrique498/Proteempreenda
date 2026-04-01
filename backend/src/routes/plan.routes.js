57e9873756ef7d73030d6eae0ac82264eb339668
Henrique498 <riccksilva50@gmail.com>
2026-04-01 10:30:59 -0300

    Delete backend directory

--- backend/src/routes/plan.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const planController = require('../controllers/plan.controller');
-
-const router = express.Router();
-
-router.get('/', planController.list);
-router.get('/:slug', planController.getBySlug);
-
-module.exports = router;
--- backend/src/services/payment.service.js
+++
@@ -1,9 +0,0 @@
-const paymentRepository = require('../repositories/payment.repository');
-
-async function listMyPayments(userId) {
-  return paymentRepository.listByUserId(userId);
-}
-
-module.exports = {
-  listMyPayments
-};
