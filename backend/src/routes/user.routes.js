869583a91cb6190516eca31f24d8835abff45f6b
Henrique498 <riccksilva50@gmail.com>
2026-04-01 10:30:17 -0300

    refazer backend

--- backend/src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const userController = require('../controllers/user.controller');
-const { requireAuth } = require('../middlewares/auth');
-
-const router = express.Router();
-
-router.get('/me', requireAuth, userController.me);
-
-module.exports = router;
